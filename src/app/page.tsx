7239cb1150b2d14afe49b4ac21b5b375c889bd44
kelvinpraises <kelvinpraises@gmail.com>
2023-03-21 05:29:30 +0100

    feat: add auto-caption

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,56 @@
 "use client";
 import Link from "next/link";
-import Image from "next/image";
 import { useState } from "react";
-import VideoPlay from "@/component/VideoPlay";
 
 export default function Home() {
-  const [search, setSearch] = useState("");
+  const [url, setUrl] = useState("");
 
   return (
     <div className="  w-full h-[calc(100vh-5rem)] overflow-y-scroll flex flex-col items-center">
-      {!search ? (
-        <div className=" w-[650px] flex flex-col gap-12 mt-5 items-center">
-          <p className=" font-imprima text-4xl text-white">
-            Auto Caption YouTube Videos
-          </p>
-          <div className=" flex w-full bg-[#404249c3] p-[6px] rounded-lg">
-            <input
-              type="text"
-              className=" w-full h-[60px] bg-[#131314] font-light focus:outline-none p-2 rounded-[3px]"
-            />
-            <button
-              className=" w-[120px] grid place-items-center cur"
-              onClick={() => setSearch("ffhgh")}
-            >
-              <p className=" text-lg">Start</p>
-            </button>
-          </div>
-          <p className=" font-semibold text-sm">
-            Login to see your encrypted History and Notes
-          </p>
-          <div className=" flex gap-[50px] items-center mt-2.5">
-            <Link href={"/history"}>
-              <div className=" flex items-center gap-[10px]">
-                <img src="/Activity.svg" alt="history" />
-                <p className=" font-semibold">History</p>
-              </div>
-            </Link>
-            <Link href={"/notes"}>
-              <div className=" flex items-center gap-[10px]">
-                <img src="/Paper.svg" alt="notes" />
-                <p className=" font-semibold">Notes</p>
-              </div>
-            </Link>
-          </div>
-          <img
-            src="/blur.svg"
-            className=" absolute top-5 z-[-10] left-[545.73px]"
-            alt=""
+      <div className=" w-[650px] flex flex-col gap-12 mt-5 items-center">
+        <p className=" font-imprima text-4xl text-white">
+          Auto Caption YouTube Videos
+        </p>
+
+        <div className=" flex w-full bg-[#404249c3] p-[6px] rounded-lg">
+          <input
+            type="text"
+            className=" w-full h-[60px] bg-[#131314] font-light focus:outline-none p-2 rounded-[3px]"
           />
+
+          <button
+            className=" w-[120px] grid place-items-center cur"
+          >
+            <p className=" text-lg">Start</p>
+          </button>
         </div>
-      ) : (
-        <VideoPlay setSearch={setSearch} link={"/"} />
-      )}
+
+        <p className=" font-semibold text-sm">
+          Login to see your encrypted History and Notes
+        </p>
+
+        <div className=" flex gap-[50px] items-center mt-2.5">
+          <Link href={"/history"}>
+            <div className=" flex items-center gap-[10px]">
+              <img src="/Activity.svg" alt="history" />
+              <p className=" font-semibold">History</p>
+            </div>
+          </Link>
+
+          <Link href={"/notes"}>
+            <div className=" flex items-center gap-[10px]">
+              <img src="/Paper.svg" alt="notes" />
+              <p className=" font-semibold">Notes</p>
+            </div>
+          </Link>
+        </div>
+
+        <img
+          src="/blur.svg"
+          className=" absolute top-5 z-[-10] left-[545.73px]"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
