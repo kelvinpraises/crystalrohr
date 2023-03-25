e22e5b95956803e8a55db718e21ba9ba402d647f
Ayo <omotolatomi01@gmail.com>
2023-03-25 18:02:56 +0100

    feat: work on comment section

--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import VideoComment from "@/component/VideoComment";
 import useCanvas from "@/hooks/useCanvas";
 import { useStore } from "@/store/useStore";
 import poll from "@/utils/poll";
@@ -105,8 +106,8 @@ export default function Page() {
       >
         Capture Image
       </button>
-      <div className="  w-full h-[calc(100vh-5rem)] overflow-y-scroll flex flex-col items-center">
-        <div className=" flex flex-col w-[740px] justify-between gap-5 mt-5">
+      <div className="  w-full h-[calc(100vh-5rem)] flex justify-center">
+        <div className=" flex flex-col gap-5 mt-5">
           <div className="flex gap-4 z-[1] items-center w-full">
             <Link href={"/"}>
               <img src="/arrowBack.svg" alt="go back" />
@@ -115,21 +116,22 @@ export default function Page() {
               {title}
             </p>
           </div>
-          <div className=" flex justify-center w-full relative">
-            <div
-              className={`absolute`}
-              ref={slicedRef}
-              style={{ display: "flex", flexWrap: "wrap" }}
-            ></div>
-            <div
-              className={` absolute top-0 w-[830px] h-[450px] overflow-hidden cursor-pointer blur-3xl backdrop-blur-2xl`}
-              style={{
-                backgroundColor: "rgba(255, 255, 255, 0.05)",
-              }}
-            />
+          <div className=" flex gap-5">
+            <div className=" flex gap-5 flex-col w-[740px]">
+              <div className=" flex justify-center w-full relative">
+                <div
+                  className={`absolute flex flex-wrap`}
+                  ref={slicedRef}
+                ></div>
+                <div
+                  className={` absolute top-0 w-[830px] h-[450px] overflow-hidden cursor-pointer blur-3xl backdrop-blur-2xl`}
+                  style={{
+                    backgroundColor: "rgba(255, 255, 255, 0.05)",
+                  }}
+                />
 
-            <div className=" flex justify-center items-center relative w-full h-[405px] overflow-hidden cursor-pointer rounded-lg">
-              <video
+                <div className=" flex justify-center items-center relative w-full h-[405px] overflow-hidden cursor-pointer rounded-lg bg-red-100">
+                  <video
                 crossOrigin="anonymous"
                 ref={videoRef}
                 height={405}
@@ -142,14 +144,14 @@ export default function Page() {
                   src={"https://web-production-44900.up.railway.app/" + link}
                 />
               </video>
-            </div>
-          </div>
+                </div>
+              </div>
 
-          <div className=" w-full p-4 gap-4 flex h-[66px] bg-[#2B2B30] items-center backdrop-blur-2xl rounded-lg">
-            <div className=" flex gap-4 items-center pr-4 border-r border-black py-1">
-              <p>Auto Caption</p>
-              <label className="relative inline-flex items-center cursor-pointer">
-                <input
+              <div className=" w-full p-4 gap-4 flex h-[66px] bg-[#2B2B30] items-center backdrop-blur-2xl rounded-lg">
+                <div className=" flex gap-4 items-center pr-4 border-r border-black py-1">
+                  <p>Auto Caption</p>
+                  <label className="relative inline-flex items-center cursor-pointer">
+                    <input
                   type="checkbox"
                   defaultChecked={startCaption}
                   onChange={() => setStartCaption((x) => !x)}
@@ -157,23 +159,26 @@ export default function Page() {
                   name="Auto caption"
                   className="sr-only peer"
                 />
-                <div className="w-11 h-6 bg-black rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-700"></div>
-              </label>
-            </div>
+                    <div className="w-11 h-6 bg-black rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-700"></div>
+                  </label>
+                </div>
 
-            <div className=" flex gap-4 items-center pr-4 border-r border-black py-1">
-              <p>Query scene</p>
-              <div className=" w-[34px] h-[34px] bg-black grid place-items-center rounded-full">
-                <img src="/Voice.svg" alt="speak" />
+                <div className=" flex gap-4 items-center pr-4 border-r border-black py-1">
+                  <p>Query scene</p>
+                  <div className=" w-[34px] h-[34px] bg-black grid place-items-center rounded-full">
+                    <img src="/Voice.svg" alt="speak" />
+                  </div>
+                </div>
+
+                <div className=" flex gap-4 items-center pr-4 border-r border-black py-1">
+                  <p>Take a note</p>
+                  <div className=" w-[34px] h-[34px] bg-black grid place-items-center rounded-full">
+                    <img src="/Paper.svg" alt="note" />
+                  </div>
+                </div>
               </div>
             </div>
-
-            <div className=" flex gap-4 items-center pr-4 border-r border-black py-1">
-              <p>Take a note</p>
-              <div className=" w-[34px] h-[34px] bg-black grid place-items-center rounded-full">
-                <img src="/Paper.svg" alt="note" />
-              </div>
-            </div>
+            <VideoComment />
           </div>
         </div>
       </div>
