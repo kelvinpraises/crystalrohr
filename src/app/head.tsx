a795bd0a3a97c0966761b6fe05e20a0e36bd4942
kelvinpraises <kelvinpraises@gmail.com>
2023-03-26 15:46:25 +0100

    feat: update head

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Crystalrohr</title>
       <link rel="icon" href="/favicon.ico" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Connecting Art Through Derivatives!" />
+      <meta name="description" content="Crystalrohr is an auto caption and in-video search platform built on YouTube. It makes content accessible to those with vision disabilities." />
     </>
   );
 }
